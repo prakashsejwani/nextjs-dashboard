ad9eb2c73fb5e219fb382353f5ee019f77d04f2a
Prakash Sejwani <prakashsejwani@gmail.com>
2025-05-02 17:08:34 +0530

    Fix linting error and add metadata

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -100,6 +100,7 @@ export async function updateInvoice(
       WHERE id = ${id}
     `;
   } catch (error) {
+    console.error(error); // now error is "used"
     return { message: 'Database Error: Failed to Update Invoice.' };
   }
  
